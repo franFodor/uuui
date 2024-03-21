d36213f5a25090c5ea2a3c73543f36edd1431d82
Fran Fodor <49796718+franFodor@users.noreply.github.com>
2024-03-21 14:19:20 +0100

    Add files via upload

--- solution.py
+++ solution.py
@@ -210,6 +210,71 @@ def ucs() -> None:
     
     return 
 
+def astar() -> None:
+    print("# ASTAR")
+    # pocetno stanje
+    open = [initial(starting_node)]
+    visited = set()
+    result = None
+
+    # sortiraj pocetni node
+    open.sort(key=lambda state: state.cost)
+    
+    # dok ima nesto u open
+    while len(open) > 0:
+        # uzmi prvi element iz open
+        state = open.pop(0)
+        #print("current state:", state.name)
+        # dodaj ga u visited 
+        visited.add(state.name)
+        # provjera jesmo li u ciljnom stanju
+        if state.name in final_nodes:
+            result = state
+            break
+
+        # za svaki sljedeci prijelaz
+        for next_node in state.children:
+            #print(next_node)
+            # ako nije vec posjecen
+            if next_node[1] not in visited:
+                #print("next", next_node)
+                # dodaj ga u open
+                # preuzeto sa: https://docs.python.org/3/library/bisect.html
+                bisect.insort_left(open, Node(next_node[1], state, node_set[next_node[1]], int(next_node[2]) + state.cost))
+                #open.append(Node(next_node[1], state, node_set[next_node[1]], int(next_node[2]) + state.cost))       
+    
+    # uzmi path
+    path = result.path(result.name)
+
+    solution = ""
+    if result:
+        solution = "yes"
+    else:
+        solution = "no"
+    
+    print("[FOUND_SOLUTION]:", solution)
+    # ako je pronadjeno rjesenja ispisi dodatne informacije
+    if solution == "yes":
+        print("[STATES_VISITED]:", len(visited))
+        print("[PATH_LENGTH]:", len(path))
+        print("[TOTAL_COST]:", float(result.cost))
+
+        # ispis puta
+        print("[PATH]:",end=" ")
+        counter = 0
+        for el in reversed(path):
+            print(el, end="")
+            if counter != len(path) - 1:
+                print(" => ", end="")
+            counter = counter + 1
+
+        print()
+    
+    return 
+
+
+    return
+
 
 def main():
     args = []
@@ -244,6 +309,8 @@ def main():
         bfs()
     elif algo == "ucs":
         ucs()
+    elif algo == "astar":
+        astar()
 
     return
 
